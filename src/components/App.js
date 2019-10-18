8970eaeee0bdf92af7608cc1754f0fd88fc78af4
EC2 Default User <ec2-user@ip-172-31-33-92.ec2.internal>
2019-10-18 05:07:44 +0000

    started working with routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,31 @@
-import React, {Component} from 'react';
-
-import List from './List'
-import MyInfo from './MyInfo'
-import TodoItem from './TodoItem'
-import todosData from './todosData'
-import ListWithAddItem from './ListWithAddItem'
-import TextInput from './TextInput'
+import React, {Component} from 'react'
+import {Nav} from 'react-bootstrap'
+import {NavLink, Switch, Route} from 'react-router-dom'
 
 class App extends Component{
     constructor(){
         super()
-        this.state = {
-            todos: todosData
-        }
-        this.handleChange = this.handleChange.bind(this);
     }
-    
-    handleChange(id) {
-        this.setState(prevState=>{
-            const updatedTodos = prevState.todos.map(todo =>{
-                if (todo.id === id){
-                    todo.completed = !todo.completed
-                }
-                return todo
-            })
-            return {
-                todos: updatedTodos
-            }
-        })
-    }
-    
+
     render(){
-        const todoItems = this.state.todos.map(todo => 
-            <TodoItem key = {todo.id} todo={todo} handleChange={this.handleChange}/>
-        )
         return (
-            <div>
-                <div className = "todoList">
-                    {todoItems}
-                </div>
-                <TextInput/>
-            </div>
+           <React.Fragment>
+                <NavLink to="/react" activeClassName="/react">
+                    React
+                </NavLink>
+                <br/>
+                <NavLink to= "/">
+                    Home
+                </NavLink>
+                <Switch>
+                    <Route path="/react">
+                        <div>React</div>
+                    </Route>
+                    <Route path = "/">
+                        <div>Slam</div>
+                    </Route>
+                </Switch>
+            </React.Fragment>    
         )
     }
 }
